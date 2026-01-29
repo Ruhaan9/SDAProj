ccae11fd83963785f84c5a33d9a2f7bc0a14d5f5
Ruhaan <ruhaan313@gmail.com>
2026-01-30 01:51:09 +0500

    Update to functional logic

--- data_loader.py
+++ data_loader.py
@@ -2,23 +2,20 @@ import json
 import csv
 
 #Open the file in read mode : r
-try:
-    with open('config.json', 'r') as file:
-        data = json.load(file)
-        print("Success, Data loaded")
-except FileNotFoundError:
-    print("Error: File not foudn")
+def load_config(filename):
+    try:
+        with open('filename', 'r') as file:
+           return json.load(file) 
+    except FileNotFoundError:
+        print("Error: Json file missing")
 
+def load_data(filename):
+    try:
+        with open(filename, mode = 'r', encoding = 'utf-8') as file:
+            reader = csv.DictReader(file)
 
-filePath = 'gdp.csv'
-
-with open(filePath, mode = 'r', encoding = 'utf-8') as file:
-    reader = csv.DictReader(file)
-
-    for row in reader:
-        country = row["Country Name"]
-        code = row["Country Code"]
-        gdp_1960 = row["1960"]
-
-        print(f"{country} ({code}) had a GDP of {gdp_1960} in 1960.")
-
+            data_list = list(reader)
+            return data_list
+    except FileNotFoundError:
+        print(f"Error: CSV file missing")
+        return []
