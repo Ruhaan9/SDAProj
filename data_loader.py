0c50326d7f81da8fa939abf8a3d68cf78af49196
Ruhaan9 <ruhaan313@gmail.com>
2026-02-08 16:02:31 +0500

    Data clean empty rows

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,7 @@ import csv
 def load_config(filename):
     try:
         with open(filename, 'r') as file:
-           return json.load(file) 
+           return json.load(file)
     except FileNotFoundError:
         print("Error: Json file missing")
 
@@ -14,7 +14,8 @@ def load_data(filename):
         with open(filename, mode = 'r', encoding = 'utf-8') as file:
             reader = csv.DictReader(file)
 
-            data_list = list(reader)
+            #clean empty rows using list comprehension
+            data_list = [row for row in reader if row.get("Country Name") and row.get("Country Name").strip()]
             return data_list
     except FileNotFoundError:
         print(f"Error: CSV file missing")
